282ec370f41ac196e1a543ccd5647e95bd75551c
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-07-15 21:47:57 +0300

    wip

--- Analogy.LogViewer.WhatsApp.UnitTests/UnitTest1.cs
+++ Analogy.LogViewer.WhatsApp.UnitTests/UnitTest1.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Analogy.LogViewer.WhatsApp.UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
+        public string fileName = "Test1.txt";
         [TestMethod]
-        public void TestMethod1()
+        public  async Task TestMethod1()
         {
+            WhatsAppTextLogFileLoader parser = new WhatsAppTextLogFileLoader();
+            CancellationTokenSource cts = new CancellationTokenSource();
+            MessageHandlerForTesting forTesting = new MessageHandlerForTesting();
+            var messages = await parser.Process(fileName, cts.Token, forTesting);
+            Assert.IsTrue(messages.Count() == 2);
         }
     }
 }
--- Analogy.LogViewer.WhatsApp/WhatsAppTextLogFileLoader.cs
+++ Analogy.LogViewer.WhatsApp/WhatsAppTextLogFileLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Analogy.DataProviders.Extensions;
@@ -10,48 +11,34 @@ namespace Analogy.LogViewer.WhatsApp
 {
     public class WhatsAppTextLogFileLoader
     {
-        private ILogParserSettings _logFileSettings;
-        private GeneralFileParser _parser;
+        private string source = "";
+        //private ILogParserSettings _logFileSettings;
+        //private GeneralFileParser _parser;
+        public WhatsAppTextLogFileLoader()
+        {
+            
+        }
         public WhatsAppTextLogFileLoader(ILogParserSettings logFileSettings)
         {
-            _logFileSettings = logFileSettings;
-            _parser = new GeneralFileParser(_logFileSettings);
+            //_logFileSettings = logFileSettings;
+            //_parser = new GeneralFileParser(_logFileSettings);
         }
-        public async Task<IEnumerable<AnalogyLogMessage>> Process(string fileName, CancellationToken token, ILogMessageCreatedHandler messagesHandler)
+
+        public async Task<IEnumerable<AnalogyLogMessage>> Process(string fileName, CancellationToken token,
+            ILogMessageCreatedHandler messagesHandler)
         {
             if (string.IsNullOrEmpty(fileName))
             {
                 AnalogyLogMessage empty = new AnalogyLogMessage($"File is null or empty. Aborting.",
-                    AnalogyLogLevel.Critical, AnalogyLogClass.General, "Analogy", "None")
+                    AnalogyLogLevel.Critical, AnalogyLogClass.General, source, "None")
                 {
-                    Source = "Analogy",
+                    Source = source,
                     Module = System.Diagnostics.Process.GetCurrentProcess().ProcessName
                 };
-                messagesHandler.AppendMessage(empty, Utils.GetFileNameAsDataSource(fileName));
-                return new List<AnalogyLogMessage> { empty };
+                messagesHandler.AppendMessage(empty, GetFileNameAsDataSource(fileName));
+                return new List<AnalogyLogMessage> {empty};
             }
-            if (!_logFileSettings.IsConfigured)
-            {
-                AnalogyLogMessage empty = new AnalogyLogMessage($"File Parser is not configured. Please set it first in the settings Window",
-                    AnalogyLogLevel.Critical, AnalogyLogClass.General, "Analogy", "None")
-                {
-                    Source = "Analogy",
-                    Module = System.Diagnostics.Process.GetCurrentProcess().ProcessName
-                };
-                messagesHandler.AppendMessage(empty, Utils.GetFileNameAsDataSource(fileName));
-                return new List<AnalogyLogMessage> { empty };
-            }
-            if (!_logFileSettings.CanOpenFile(fileName))
-            {
-                AnalogyLogMessage empty = new AnalogyLogMessage($"File {fileName} Is not supported or not configured correctly in the windows settings",
-                    AnalogyLogLevel.Critical, AnalogyLogClass.General, "Analogy", "None")
-                {
-                    Source = "Analogy",
-                    Module = System.Diagnostics.Process.GetCurrentProcess().ProcessName
-                };
-                messagesHandler.AppendMessage(empty, Utils.GetFileNameAsDataSource(fileName));
-                return new List<AnalogyLogMessage> { empty };
-            }
+
             List<AnalogyLogMessage> messages = new List<AnalogyLogMessage>();
             try
             {
@@ -59,34 +46,109 @@ namespace Analogy.LogViewer.WhatsApp
                 {
                     using (var reader = new StreamReader(stream))
                     {
+                        string firstLine = string.Empty;
+                        string otherdata = string.Empty;
                         while (!reader.EndOfStream)
                         {
-                            var line = await reader.ReadLineAsync();
-                            var items = line.Split(_parser.splitters, StringSplitOptions.RemoveEmptyEntries);
-                            while (items.Length < _logFileSettings.ValidItemsCount)
+                            if (string.IsNullOrEmpty(firstLine))
+                                firstLine = await reader.ReadLineAsync();
+                            if (reader.EndOfStream)
                             {
-                                line = line + Environment.NewLine + await reader.ReadLineAsync();
-                                items = line.Split(_parser.splitters, StringSplitOptions.RemoveEmptyEntries);
+
+                                var m = Parse(firstLine);
+                                messages.Add(m);
+                                messagesHandler.AppendMessage(m, GetFileNameAsDataSource(fileName));
+                                continue;
                             }
-                            var entry = _parser.Parse(line);
-                            messages.Add(entry);
+
+                            var line = await reader.ReadLineAsync();
+
+                            if (line.Contains(" - "))
+                            {
+                                string lineToProcess = string.IsNullOrEmpty(firstLine)
+                                    ? otherdata
+                                    : firstLine + Environment.NewLine + otherdata;
+                                var m = Parse(lineToProcess);
+                                messages.Add(m);
+                                messagesHandler.AppendMessage(m, GetFileNameAsDataSource(fileName));
+                                firstLine = line;
+                                otherdata = string.Empty;
+                            }
+                            else
+                            {
+                                otherdata += line + Environment.NewLine;
+                            }
+                        }
+
+                        if (!string.IsNullOrEmpty(firstLine))
+                        {
+                            var m = Parse(firstLine);
+                            messages.Add(m);
+                            messagesHandler.AppendMessage(m, GetFileNameAsDataSource(fileName));
                         }
                     }
                 }
-                messagesHandler.AppendMessages(messages, fileName);
+
                 return messages;
             }
             catch (Exception e)
             {
-                AnalogyLogMessage empty = new AnalogyLogMessage($"Error occured processing file {fileName}. Reason: {e.Message}",
-                    AnalogyLogLevel.Critical, AnalogyLogClass.General, "Analogy", "None")
+                AnalogyLogMessage empty = new AnalogyLogMessage(
+                    $"Error occured processing file {fileName}. Reason: {e.Message}",
+                    AnalogyLogLevel.Critical, AnalogyLogClass.General, source, "None")
                 {
-                    Source = "Analogy",
+                    Source = source,
                     Module = System.Diagnostics.Process.GetCurrentProcess().ProcessName
                 };
-                messagesHandler.AppendMessage(empty, Utils.GetFileNameAsDataSource(fileName));
-                return new List<AnalogyLogMessage> { empty };
+                messagesHandler.AppendMessage(empty, GetFileNameAsDataSource(fileName));
+                return new List<AnalogyLogMessage> {empty};
             }
+
+
+        }
+
+        private AnalogyLogMessage Parse(string line)
+        {
+            int first = line.IndexOf(" - ", StringComparison.InvariantCultureIgnoreCase);
+            AnalogyLogMessage m = new AnalogyLogMessage();
+            m.Source = source;
+            m.Module = source;
+            string datetime = line.Substring(0, first);
+            if (DateTime.TryParse(datetime, out DateTime dateVal))
+            {
+                m.Date = dateVal;
+            }
+
+            string sub = line.Substring(first + 1);
+            int firstSpace = sub.IndexOf(':');
+            string level = sub.Substring(0, firstSpace);
+            if (level.StartsWith("Info"))
+            {
+                m.Level = AnalogyLogLevel.Event;
+            }
+            else if (level.StartsWith("Debug"))
+            {
+                m.Level = AnalogyLogLevel.Debug;
+            }
+            else if (level.StartsWith("Warning"))
+            {
+                m.Level = AnalogyLogLevel.Warning;
+            }
+
+            sub = sub.Substring(level.Length);
+            //int sourceIndex = sub.IndexOf(']') + 2;
+            //m.Source = sub.Substring(2, sourceIndex);
+            m.Source = source;
+            m.Text = sub; //.Substring(sourceIndex);
+            return m;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string GetFileNameAsDataSource(string fileName)
+        {
+            string file = Path.GetFileName(fileName);
+            return fileName.Equals(file) ? fileName : $"{file} ({fileName})";
+
         }
     }
 }
