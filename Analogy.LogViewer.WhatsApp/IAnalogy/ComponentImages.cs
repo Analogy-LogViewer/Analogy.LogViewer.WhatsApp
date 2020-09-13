f8a85763f692a3d921c090d939fe73d3470a6871
Lior Banai <liorb@kama-medical.com>
2020-09-13 10:10:20 +0300

    update nguet

--- Analogy.LogViewer.WhatsApp/IAnalogy/ComponentImages.cs
+++ Analogy.LogViewer.WhatsApp/IAnalogy/ComponentImages.cs
@@ -5,28 +5,28 @@ using Analogy.LogViewer.WhatsApp.Properties;
 
 namespace Analogy.LogViewer.WhatsApp.IAnalogy
 {
-    public class ComponentImages : IAnalogyComponentImages
-    {
-        public Image GetLargeImage(Guid analogyComponentId)
-        {
-            if (analogyComponentId == WhatsAppTextParserFactory.AnalogyWhatsAppGuid)
-                return Resources.whatsappicon32x32;
-            return null;
-        }
+    //public class ComponentImages : IAnalogyComponentImages
+    //{
+    //    public Image GetLargeImage(Guid analogyComponentId)
+    //    {
+    //        if (analogyComponentId == WhatsAppTextParserFactory.AnalogyWhatsAppGuid)
+    //            return Resources.whatsappicon32x32;
+    //        return null;
+    //    }
 
-        public Image GetSmallImage(Guid analogyComponentId)
-        {
-            if (analogyComponentId == WhatsAppTextParserFactory.AnalogyWhatsAppGuid)
-                return Resources.whatsappicon16x16;
-            return null;
-        }
+    //    public Image GetSmallImage(Guid analogyComponentId)
+    //    {
+    //        if (analogyComponentId == WhatsAppTextParserFactory.AnalogyWhatsAppGuid)
+    //            return Resources.whatsappicon16x16;
+    //        return null;
+    //    }
 
-        public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => null;
+    //    public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => null;
 
-        public Image GetOnlineConnectedSmallImage(Guid analogyComponentId) => null;
+    //    public Image GetOnlineConnectedSmallImage(Guid analogyComponentId) => null;
 
-        public Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId) => null;
+    //    public Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId) => null;
 
-        public Image GetOnlineDisconnectedSmallImage(Guid analogyComponentId) => null;
-    }
+    //    public Image GetOnlineDisconnectedSmallImage(Guid analogyComponentId) => null;
+    //}
 }
